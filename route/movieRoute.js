cc84a2fd9e654f966785cb2c92acf330ad79b68a
ck <ckpal9727@gmail.com>
2022-11-29 18:04:03 +0530

    Fix routefor getting movie by type and all

--- route/movieRoute.js
+++ route/movieRoute.js
@@ -23,25 +23,49 @@ const upload = multer({ storage: storage })
 // 2.To get by type
 // 3.To get by id single movie
 route.get('/', async (req, res) => {
-    if (req.query.type) {
-        try {
-            const comedyMovie = await Movie.find({ m_type: req.query.type })
-            res.json({ comedyMovie });
-        } catch (error) {
-            console.log(error)
-        }
-    } else if (req.query.id) {
-        const id = req.query.id
-        const movie = await Movie.findById({ _id: id })
-        res.json(movie)
-    } else {
-        try {
-            const movie = await Movie.find();
-            res.json({ movie });
-        } catch (error) {
-            console.log(error)
-        }
+    // if (req.query.type) {
+    //     try {
+    //         const comedyMovie = await Movie.find({ m_type: req.query.type })
+    //         res.json({ comedyMovie });
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // } else if (req.query.id) {
+    //     const id = req.query.id
+    //     const movie = await Movie.findById({ _id: id })
+    //     res.json(movie)
+    // } else {
+    //     try {
+    //         const movie = await Movie.find();
+    //         res.json({ movie });
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+    // type= horror,comedy,action,scifi,romance,thriller,animation,drama,webshow
+    const horror=await Movie.find({m_type:"horror"}); 
+    const comedy=await Movie.find({m_type:"comedy"}); 
+    const action=await Movie.find({m_type:"action"}); 
+    const scifi=await Movie.find({m_type:"scifi"}); 
+    const romance=await Movie.find({m_type:"romance"}); 
+    const thriller=await Movie.find({m_type:"thriller"}); 
+    const animation=await Movie.find({m_type:"animation"}); 
+    const drama=await Movie.find({m_type:"drama"}); 
+    const webshow=await Movie.find({m_type:"webshow"}); 
+
+    const movies={
+        horror:horror,
+        comedy:comedy,
+        action:action,
+        scifi:scifi,
+        romance:romance,
+        thriller:thriller,
+        animation:animation,
+        drama:drama,
+        webshow:webshow,
     }
+    
+    res.json({movies:movies});
 })
 
 // To upload movie
